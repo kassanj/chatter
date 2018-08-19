9e862501bccf11389eccc9b903b6306fdf8bb65d
kassanj <kassandra@whereby.us>
2018-08-19 13:41:09 -0700

    include addTweet and handleAddTweet actions

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,16 @@
 import { saveLikeToggle } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
 export const TOGGLE_TWEET = 'TOGGLE_TWEET'
+export const ADD_TWEET = 'ADD_TWEET'
+
+function addTweet (tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet
+  }
+}
 
 export function receiveTweets (tweets) {
   return {
@@ -19,9 +28,26 @@ function toggleTweet ({ id, authedUser, hasLiked }) {
   }
 }
 
+
+export function handleAddTweet (text, replyingTo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+    
+     dispatch(showLoading())
+
+     return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo
+    })
+      .then((tweet) => dispatch(addTweet(tweet)))
+      .then(() => dispatch(hideLoading()))
+  }
+}
+
 export function handleToggleTweet (info) {
   return (dispatch) => {
-    
+
     dispatch(toggleTweet(info))
 
     return saveLikeToggle(info)
